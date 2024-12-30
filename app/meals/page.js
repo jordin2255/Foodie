a97002d6641819120eb2cf6436929ad6bf711780
jordin <jordinmathew.mec@gmail.com>
2024-12-30 13:32:43 +1000

    added meals component

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,20 @@
+import MealsGrid from "@/components/meals/meal-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious<span className={classes.highlight}>Meals</span>
+        </h1>
+        <p>Choose your favorite</p>
+        <p className={classes.cta}>
+        <Link href="/meals/share">share</Link></p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]}/>
+      </main>
+    </>
+  );
 }
--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
+
 import logoImage from "@/assets/logo.png";
 import MainHeaderBackground from '@/components/main-header/main-header-bg';
 import classes from './main-header.module.css';
+import NavLink from "./nav-link";
+
 export default function MainHeader() {
+  
   return (
     <>
     <MainHeaderBackground />
@@ -15,10 +19,10 @@ export default function MainHeader() {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href="/meals">Browswe Meals</Link>
+           <NavLink href="/meals">Meals</NavLink>
           </li>
           <li>
-          <Link href="/community">Foodies Community</Link>
+          <NavLink href="/community">Community</NavLink>
           </li>
         </ul>
       </nav>
